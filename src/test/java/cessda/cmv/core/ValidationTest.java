82ee9541a2a4e4b444a04a6d02eed962185556b8
Alexander Mühlbauer <alexander.muehlbauer@gesis.org>
2020-02-01 18:41:44 +0100

    ValidationTest -> Check which required xpaths are not in metadata document

--- src/test/java/cessda/cmv/core/ValidationTest.java
+++ src/test/java/cessda/cmv/core/ValidationTest.java
@@ -1,6 +1,8 @@
 package cessda.cmv.core;
 
+import static java.lang.Boolean.FALSE;
 import static org.gesis.commons.resource.Resource.newResource;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
@@ -8,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.gesis.commons.resource.Resource;
 import org.gesis.commons.resource.TextResource;
@@ -22,16 +25,33 @@ public class ValidationTest
 	public void inspectDocuments() throws IOException
 	{
 		DomDocument.V10 metadataDocument = newDocument( new File( "src/test/resources/ddi-v25/ukds-7481.xml" ) );
-		assertThat( metadataDocument.getElementXPaths(), hasSize( 73 ) );
+		List<String> doucmentXPaths = metadataDocument.getElementXPaths();
+		assertThat( doucmentXPaths, hasSize( 73 ) );
 
 		DomDocument.V10 profileDocument = newDocument( new File( "src/test/resources/ddi-v25/cdc_profile.xml" ) );
 		assertThat( profileDocument.getElementXPaths(), hasSize( 17 ) );
-		List<Node> nodes = profileDocument.selectNodes( "/DDIProfile/Used/@xpath" );
-		assertThat( nodes, hasSize( 88 ) );
-		nodes = profileDocument.selectNodes( "/DDIProfile/Used[@isRequired='true']/@xpath" );
-		assertThat( nodes, hasSize( 42 ) );
-		nodes = profileDocument.selectNodes( "/DDIProfile/Used[@isRequired='false']/@xpath" );
-		assertThat( nodes, hasSize( 88 - 42 ) );
+		List<String> xpaths = profileDocument.selectNodes( "/DDIProfile/Used/@xpath" ).stream()
+				.map( Node::getTextContent )
+				.collect( Collectors.toList() );
+		assertThat( xpaths, hasSize( 88 ) );
+
+		// required xpaths
+		List<String> requiredXPaths = profileDocument.selectNodes( "/DDIProfile/Used[@isRequired='true']/@xpath" )
+				.stream()
+				.map( Node::getTextContent )
+				.collect( Collectors.toList() );
+		assertThat( requiredXPaths, hasSize( 42 ) );
+		xpaths = requiredXPaths.stream()
+				.filter( requiredXPath -> !doucmentXPaths.contains( requiredXPath ) )
+				.collect( Collectors.toList() );
+		boolean isValid = xpaths.isEmpty();
+		assertThat( isValid, equalTo( FALSE ) );
+
+		// not required xpaths
+		xpaths = profileDocument.selectNodes( "/DDIProfile/Used[@isRequired='false']/@xpath" ).stream()
+				.map( Node::getTextContent )
+				.collect( Collectors.toList() );
+		assertThat( xpaths, hasSize( 88 - 42 ) );
 	}
 
 	private DomDocument.V10 newDocument( File file )
