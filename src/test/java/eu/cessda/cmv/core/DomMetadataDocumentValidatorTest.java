6db8d1abe88e3e59b4e01b0ddb32113700c11e8e
Alexander Mühlbauer <alexander.muehlbauer@gesis.org>
2020-02-13 17:52:56 +0100

    DomMetadataDocumentValidatorTest -> Assert xml wellformedness as precondition

--- src/test/java/eu/cessda/cmv/core/DomMetadataDocumentValidatorTest.java
+++ src/test/java/eu/cessda/cmv/core/DomMetadataDocumentValidatorTest.java
@@ -1,15 +1,17 @@
 package eu.cessda.cmv.core;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.File;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.gesis.commons.xml.XmlNotWellformedException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 public class DomMetadataDocumentValidatorTest
 {
@@ -32,6 +34,19 @@ public class DomMetadataDocumentValidatorTest
 		assertThat( constraintViolations.stream()
 				.filter( cv -> cv instanceof RecommendedNodeConstraintViolation )
 				.collect( Collectors.toList() ), hasSize( 25 ) );
-		assertFalse( constraintViolations.isEmpty() );
+	}
+
+	@Test
+	public void constructWithNotWellformedMetadataDocument()
+	{
+		// given
+		URI documentUri = new File( "src/test/resources/ddi-v25/ukds-7481-not-wellformed.xml" ).toURI();
+		URI profileUri = new File( "src/test/resources/ddi-v25/cdc_profile.xml" ).toURI();
+
+		// when
+		Executable executable = () -> new DomMetadataDocumentValidator( documentUri, profileUri );
+
+		// then
+		assertThrows( XmlNotWellformedException.class, executable );
 	}
 }
