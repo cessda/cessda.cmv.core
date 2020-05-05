16f74de1e6952c1886b1571301450fa1c3e80ccd
Alexander Mühlbauer <alexander.muehlbauer@gesis.org>
2020-05-05 21:31:57 +0200

    StrictValidationGateTest -> Separate from StandardValidationGateTest

--- src/test/java/eu/cessda/cmv/core/StrictValidationGateTest.java
+++ src/test/java/eu/cessda/cmv/core/StrictValidationGateTest.java
@@ -2,13 +2,8 @@ package eu.cessda.cmv.core;
 
 import static eu.cessda.cmv.core.Factory.newDocument;
 import static eu.cessda.cmv.core.Factory.newProfile;
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,33 +11,8 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-public class StandardValidationGateTest
+public class StrictValidationGateTest
 {
-	@Test
-	public void validate()
-	{
-		// given
-		Document.V10 document = mock( Document.V10.class );
-		when( document.getNodes( anyString() ) ).thenReturn( emptyList() );
-		Profile.V10 profile = mock( Profile.V10.class );
-		when( profile.getConstraints() ).thenReturn( asList(
-				new MandatoryNodeConstraint( "/path/to/mandatory/node" ),
-				new RecommendedNodeConstraint( "/path/to/recommended/node" ) ) );
-
-		// when
-		ValidationGate.V10 validatationGate = new StandardValidationGate();
-		List<ConstraintViolation> constraintViolations = validatationGate.validate( document, profile );
-
-		// then
-		assertThat( constraintViolations, hasSize( 2 ) );
-		assertThat( constraintViolations.stream()
-				.filter( cv -> cv.getMessage().contains( "mandatory" ) )
-				.collect( Collectors.toList() ), hasSize( 1 ) );
-		assertThat( constraintViolations.stream()
-				.filter( cv -> cv.getMessage().contains( "recommended" ) )
-				.collect( Collectors.toList() ), hasSize( 1 ) );
-	}
-
 	@Test
 	public void validate_cdc_ukds7481() throws IOException
 	{
@@ -51,17 +21,21 @@ public class StandardValidationGateTest
 		Profile profile = newProfile( getClass().getResource( "/ddi-v25/cdc25_profile.xml" ) );
 
 		// when
-		ValidationGate.V10 validationGate = new StandardValidationGate();
+		ValidationGate.V10 validationGate = new StrictValidationGate();
 		List<ConstraintViolation> constraintViolations = validationGate.validate( document, profile );
 
 		// then
-		assertThat( constraintViolations, hasSize( 19 ) );
+		assertThat( constraintViolations, hasSize( 40 ) );
 		assertThat( constraintViolations.stream()
 				.filter( cv -> cv.getMessage().contains( "mandatory" ) )
 				.collect( Collectors.toList() ), hasSize( 10 ) );
 		assertThat( constraintViolations.stream()
 				.filter( cv -> cv.getMessage().contains( "recommended" ) )
 				.collect( Collectors.toList() ), hasSize( 9 ) );
+		assertThat( constraintViolations.stream()
+				.filter( cv -> cv.getMessage().contains( "optional" ) )
+				.collect( Collectors.toList() ), hasSize( 21 ) );
+
 	}
 
 	@Test
@@ -72,17 +46,20 @@ public class StandardValidationGateTest
 		Profile profile = newProfile( getClass().getResource( "/ddi-v25/cdc25_profile.xml" ) );
 
 		// when
-		ValidationGate.V10 validationGate = new StandardValidationGate();
+		ValidationGate.V10 validationGate = new StrictValidationGate();
 		List<ConstraintViolation> constraintViolations = validationGate.validate( document, profile );
 
 		// then
-		assertThat( constraintViolations, hasSize( 18 ) );
+		assertThat( constraintViolations, hasSize( 40 ) );
 		assertThat( constraintViolations.stream()
 				.filter( cv -> cv.getMessage().contains( "mandatory" ) )
 				.collect( Collectors.toList() ), hasSize( 9 ) );
 		assertThat( constraintViolations.stream()
 				.filter( cv -> cv.getMessage().contains( "recommended" ) )
 				.collect( Collectors.toList() ), hasSize( 9 ) );
+		assertThat( constraintViolations.stream()
+				.filter( cv -> cv.getMessage().contains( "optional" ) )
+				.collect( Collectors.toList() ), hasSize( 22 ) );
 	}
 
 	@Test
@@ -94,16 +71,19 @@ public class StandardValidationGateTest
 		Profile profile = newProfile( getClass().getResource( "/ddi-v25/cdc25_profile.xml" ) );
 
 		// when
-		ValidationGate.V10 validationGate = new StandardValidationGate();
+		ValidationGate.V10 validationGate = new StrictValidationGate();
 		List<ConstraintViolation> constraintViolations = validationGate.validate( document, profile );
 
 		// then
-		assertThat( constraintViolations, hasSize( 13 ) );
+		assertThat( constraintViolations, hasSize( 30 ) );
 		assertThat( constraintViolations.stream()
 				.filter( cv -> cv.getMessage().contains( "mandatory" ) )
 				.collect( Collectors.toList() ), hasSize( 8 ) );
 		assertThat( constraintViolations.stream()
 				.filter( cv -> cv.getMessage().contains( "recommended" ) )
 				.collect( Collectors.toList() ), hasSize( 5 ) );
+		assertThat( constraintViolations.stream()
+				.filter( cv -> cv.getMessage().contains( "optional" ) )
+				.collect( Collectors.toList() ), hasSize( 17 ) );
 	}
 }
