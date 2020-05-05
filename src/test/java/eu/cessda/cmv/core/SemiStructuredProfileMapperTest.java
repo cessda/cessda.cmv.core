064b271e418eb57f94ade2f1b6316bc7b14fef84
Alexander Mühlbauer <alexander.muehlbauer@gesis.org>
2020-05-05 22:42:13 +0200

    SemiStructuredProfileMapperTest -> Print mapped profile to console

--- src/test/java/eu/cessda/cmv/core/SemiStructuredProfileMapperTest.java
+++ src/test/java/eu/cessda/cmv/core/SemiStructuredProfileMapperTest.java
@@ -1,6 +1,7 @@
 package eu.cessda.cmv.core;
 
 import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
+import static org.gesis.commons.resource.Resource.newResource;
 import static org.hamcrest.io.FileMatchers.anExistingFile;
 import static org.junit.Assert.assertThat;
 
@@ -8,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import org.gesis.commons.resource.TextResource;
 import org.junit.jupiter.api.Test;
 
 public class SemiStructuredProfileMapperTest
@@ -20,5 +22,6 @@ public class SemiStructuredProfileMapperTest
 		copyInputStreamToFile( SemiStructuredProfileMapper.map( sourceUrl ), targetFile );
 
 		assertThat( targetFile, anExistingFile() );
+		System.out.println( new TextResource( newResource( targetFile ) ).toString() );
 	}
 }
