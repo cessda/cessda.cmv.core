ffed9d043152ec281d9b59f6341388f881716f26
Alexander Mühlbauer <alexander.muehlbauer@gesis.org>
2020-05-12 11:34:16 +0200

    MinimumElementOccuranceValidator -> Reduce code duplication by base class extraction

--- src/main/java/eu/cessda/cmv/core/MandatoryNodeValidator.java
+++ src/main/java/eu/cessda/cmv/core/MandatoryNodeValidator.java
@@ -1,44 +1,21 @@
 package eu.cessda.cmv.core;
 
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
-
 import java.util.Optional;
 
-import org.gesis.commons.xml.LocationInfo;
-import org.mockito.Mockito;
-
-class MandatoryNodeValidator implements Validator.V10
+class MandatoryNodeValidator extends MinimumElementOccuranceValidator
 {
 	private String locationPath;
-	private long count;
 
-	MandatoryNodeValidator( String locationPath, long count )
+	MandatoryNodeValidator( String locationPath, long actualCount )
 	{
-		this.locationPath = locationPath;
-		this.count = count;
+		super( locationPath, actualCount, 1 );
 	}
 
 	@Override
-	public Optional<ConstraintViolation> validate()
-	{
-		if ( count == 0 )
-		{
-			return of( newConstraintViolation() );
-		}
-		else
-		{
-			return empty();
-		}
-	}
-
-	private ConstraintViolation newConstraintViolation()
+	protected ConstraintViolation newConstraintViolation()
 	{
 		String message = "'%s' is mandatory";
 		message = String.format( message, locationPath );
-		LocationInfo locationInfo = Mockito.mock( LocationInfo.class );
-		Mockito.when( locationInfo.getLineNumber() ).thenReturn( 10 );
-		Mockito.when( locationInfo.getColumnNumber() ).thenReturn( 20 );
-		return new ConstraintViolation( message, Optional.of( locationInfo ) );
+		return new ConstraintViolation( message, Optional.empty() );
 	}
 }
