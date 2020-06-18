e10a4e4cd7d36d60f7d992c4b260ad6407d26752
Alexander Mühlbauer <alexander.muehlbauer@gesis.org>
2020-06-18 10:44:13 +0200

    CdcCodebookDocumentValidationTest -> Include gesis demo documents in test cases

--- src/test/java/eu/cessda/cmv/core/CdcCodebookDocumentValidationTest.java
+++ src/test/java/eu/cessda/cmv/core/CdcCodebookDocumentValidationTest.java
@@ -1,7 +1,7 @@
 package eu.cessda.cmv.core;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,12 +57,33 @@ public class CdcCodebookDocumentValidationTest
 
 		testParameter = new TestParameter();
 		testParameter.documentName = "/demo-documents/ddi-v25/fsd-3271.xml";
-		// mandatory + blank
-		testParameter.expectedViolationsAtBasicValidationGate = 8 + 1;
-		// mandatory + recommended + blank
-		testParameter.expectedViolationsAtStandardValidationGate = 8 + 6 + 3;
-		// mandatory + recommended + optional + blank
-		testParameter.expectedViolationsAtStrictValidationGate = 8 + 6 + 16 + 9;
+		// (mandatory + blank)
+		testParameter.expectedViolationsAtBasicValidationGate = (8 + 1);
+		// (mandatory + blank) + (recommended + blank)
+		testParameter.expectedViolationsAtStandardValidationGate = (8 + 1) + (6 + 2);
+		// (mandatory + blank) + (recommended + blank) + (optional + blank)
+		testParameter.expectedViolationsAtStrictValidationGate = (8 + 1) + (6 + 2) + (16 + 6);
+		testParameters.add( testParameter );
+
+		testParameter = new TestParameter();
+		testParameter.documentName = "/demo-documents/ddi-v25/gesis-5100.xml";
+		testParameter.expectedViolationsAtBasicValidationGate = 9;
+		testParameter.expectedViolationsAtStandardValidationGate = 9 + 10 + 3;
+		testParameter.expectedViolationsAtStrictValidationGate = 9 + 10 + 18 + 7;
+		testParameters.add( testParameter );
+
+		testParameter = new TestParameter();
+		testParameter.documentName = "/demo-documents/ddi-v25/gesis-5300.xml";
+		testParameter.expectedViolationsAtBasicValidationGate = (8 + 1);
+		testParameter.expectedViolationsAtStandardValidationGate = (8 + 1) + (10 + 3);
+		testParameter.expectedViolationsAtStrictValidationGate = (8 + 1) + (10 + 3) + (18 + 5);
+		testParameters.add( testParameter );
+
+		testParameter = new TestParameter();
+		testParameter.documentName = "/demo-documents/ddi-v25/gesis-2800.xml";
+		testParameter.expectedViolationsAtBasicValidationGate = 9;
+		testParameter.expectedViolationsAtStandardValidationGate = 9 + (10 + 3);
+		testParameter.expectedViolationsAtStrictValidationGate = 9 + (10 + 3) + (17 + 4);
 		testParameters.add( testParameter );
 
 		return testParameters.stream();
