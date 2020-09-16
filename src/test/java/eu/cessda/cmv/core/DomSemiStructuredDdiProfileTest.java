056498bd220dbeb5aa4dee29ec793930114ba753
Alexander Mühlbauer <alexander.muehlbauer@gesis.org>
2020-09-16 23:12:08 +0200

    DomSemiStructuredDdiProfileTest -> Make expected constraint size more explicit

--- src/test/java/eu/cessda/cmv/core/DomSemiStructuredDdiProfileTest.java
+++ src/test/java/eu/cessda/cmv/core/DomSemiStructuredDdiProfileTest.java
@@ -3,6 +3,7 @@ package eu.cessda.cmv.core;
 import static org.gesis.commons.resource.Resource.newResource;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.io.FileMatchers.anExistingFile;
 
 import java.io.File;
@@ -32,7 +33,21 @@ class DomSemiStructuredDdiProfileTest
 		Profile.V10 profile = factory.newProfile( url );
 
 		// then
-		assertThat( profile.getConstraints(), hasSize( 198 ) );
+		assertThat( countConstraints( profile, CompilableXPathConstraint.class ), is( 61 ) );
+		assertThat( countConstraints( profile, PredicatelessXPathConstraint.class ), is( 61 ) );
+		assertThat( countConstraints( profile, RecommendedNodeConstraint.class ), is( 24 ) );
+		assertThat( countConstraints( profile, MandatoryNodeConstraint.class ), is( 10 ) );
+		assertThat( countConstraints( profile, FixedValueNodeConstraint.class ), is( 5 ) );
+		assertThat( countConstraints( profile, OptionalNodeConstraint.class ), is( 27 ) );
+		assertThat( countConstraints( profile, MandatoryNodeIfParentPresentConstraint.class ), is( 15 ) );
+		assertThat( profile.getConstraints(), hasSize( 61 + 61 + 24 + 10 + 5 + 27 + 15 ) );
+	}
+
+	private int countConstraints( Profile.V10 profile, Class<? extends Constraint> clazz )
+	{
+		return (int) profile.getConstraints().stream()
+				.filter( constraint -> clazz.isInstance( constraint ) )
+				.count();
 	}
 
 	@Test
@@ -49,6 +64,6 @@ class DomSemiStructuredDdiProfileTest
 		ProfileV0 jaxbProfile = profile.toJaxbProfileV0();
 		jaxbProfile.saveAs( targetFile );
 		assertThat( targetFile, anExistingFile() );
-		assertThat( jaxbProfile.getConstraints(), hasSize( 183 ) ); // why not 198 ?
+		assertThat( jaxbProfile.getConstraints(), hasSize( 183 ) ); // why not 203 ?
 	}
 }
