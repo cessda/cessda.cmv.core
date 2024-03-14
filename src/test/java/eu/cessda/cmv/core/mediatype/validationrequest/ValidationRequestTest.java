8a96d0a45bfe67c8a9270ef13db15363249707ea
Matthew Morris <matthew.morris@cessda.eu>
2024-03-14 08:37:48 +0100

    tests → remove the usage of org.gesis.commons.resource.Resource from ValidationRequestTest

--- src/test/java/eu/cessda/cmv/core/mediatype/validationrequest/ValidationRequestTest.java
+++ src/test/java/eu/cessda/cmv/core/mediatype/validationrequest/ValidationRequestTest.java
@@ -28,8 +28,6 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import eu.cessda.cmv.core.CessdaMetadataValidatorFactory;
 import eu.cessda.cmv.core.ValidationGateName;
-import org.gesis.commons.resource.Resource;
-import org.gesis.commons.resource.TextResource;
 import org.gesis.commons.test.DefaultTestEnv;
 import org.gesis.commons.test.TestEnv;
 import org.junit.jupiter.api.Test;
@@ -39,6 +37,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -46,7 +45,6 @@ import java.util.stream.Stream;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.LOWER_CAMEL_CASE;
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.UPPER_CAMEL_CASE;
-import static org.gesis.commons.resource.Resource.newResource;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -54,8 +52,8 @@ import static org.hamcrest.Matchers.*;
 class ValidationRequestTest
 {
 	private final TestEnv.V14 testEnv;
-	private final Resource profile;
-	private final Resource document;
+	private final URL profile;
+	private final URL document;
 	private final ValidationGateName validationGateName;
 	private final Set<String> constraints;
 
@@ -63,29 +61,24 @@ class ValidationRequestTest
 	{
 		String baseUrl = "/demo-documents/ddi-v25";
 
-		URL profileResource = this.getClass().getResource( baseUrl + "/cdc25_profile.xml" );
-		URL documentResource = this.getClass().getResource( baseUrl + "/gesis-5300.xml" );
+		this.profile = this.getClass().getResource( baseUrl + "/cdc25_profile.xml" );
+		this.document = this.getClass().getResource( baseUrl + "/gesis-5300.xml" );
 
-		assert profileResource != null;
-		assert documentResource != null;
-
-		profile = new TextResource( newResource( profileResource ) );
-		document = new TextResource( newResource( documentResource ) );
 		validationGateName = ValidationGateName.BASIC;
 		constraints = CessdaMetadataValidatorFactory.getConstraints();
 		testEnv = DefaultTestEnv.newInstance( ValidationRequestTest.class );
 	}
 
-	private ValidationRequestProvider[] newValidationRequest()
+	private ValidationRequestProvider[] newValidationRequest() throws URISyntaxException
 	{
 		ValidationRequest validationRequestWithConstraints = new ValidationRequest();
-		validationRequestWithConstraints.setDocument( document.getUri() );
-		validationRequestWithConstraints.setProfile( profile.toString() );
+		validationRequestWithConstraints.setDocument( document.toURI() );
+		validationRequestWithConstraints.setProfile( profile.toURI() );
 		validationRequestWithConstraints.setConstraints( constraints );
 
 		ValidationRequest validationRequestWithGateName = new ValidationRequest();
-		validationRequestWithGateName.setDocument( document.getUri() );
-		validationRequestWithGateName.setProfile( profile.toString() );
+		validationRequestWithGateName.setDocument( document.toURI() );
+		validationRequestWithGateName.setProfile( profile.toURI() );
 		validationRequestWithGateName.setValidationGateName( validationGateName );
 
 		return new ValidationRequestProvider[]{
@@ -112,7 +105,7 @@ class ValidationRequestTest
 		validationRequestProvider.validator.accept( validationRequest );
 	}
 
-	Stream<Arguments> writeAndReadWithJackson()
+	Stream<Arguments> writeAndReadWithJackson() throws URISyntaxException
 	{
 		return Stream.of( newValidationRequest() ).flatMap( validationRequestProvider ->
 				Stream.of(
@@ -140,7 +133,7 @@ class ValidationRequestTest
 	}
 
 	@Test
-	void shouldValidateRequests()
+	void shouldValidateRequests() throws URISyntaxException
 	{
 		// Generate an invalid request
 		ValidationRequest invalidRequest = new ValidationRequest();
@@ -160,7 +153,7 @@ class ValidationRequestTest
 	}
 
 	@Test
-	void testEquality()
+	void testEquality() throws URISyntaxException
 	{
 		// Generate two identical empty requests
 		ValidationRequest firstEmptyRequest = new ValidationRequest();
@@ -171,13 +164,13 @@ class ValidationRequestTest
 
 		// Generate two identical populated requests
 		ValidationRequest firstPopulatedRequest = new ValidationRequest();
-		firstPopulatedRequest.setDocument( document.getUri() );
-		firstPopulatedRequest.setProfile( profile.toString() );
+		firstPopulatedRequest.setDocument( document.toURI() );
+		firstPopulatedRequest.setProfile( profile.toURI() );
 		firstPopulatedRequest.setConstraints( constraints );
 
 		ValidationRequest secondPopulatedRequest = new ValidationRequest();
-		secondPopulatedRequest.setDocument( document.getUri() );
-		secondPopulatedRequest.setProfile( profile.toString() );
+		secondPopulatedRequest.setDocument( document.toURI() );
+		secondPopulatedRequest.setProfile( profile.toURI() );
 		secondPopulatedRequest.setConstraints( constraints );
 
 		// Should be equal
